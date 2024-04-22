4b2a49b07fa82a92dd58baf277f1675d7982c512
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2024-04-22 07:46:54 +0000

    content access

--- web/src/app/(dashboard)/layout.tsx
+++ web/src/app/(dashboard)/layout.tsx
@@ -1,9 +1,15 @@
+import { getAccess } from "@/modules/auth/lib/get-access/get-access";
 import { MainDashboardLayout } from "./components/main-dashboard-layout/main-dashboard-layout";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <MainDashboardLayout>{children}</MainDashboardLayout>;
+  const access = await getAccess();
+  return (
+    <MainDashboardLayout hasAccess={!!access.content}>
+      {children}
+    </MainDashboardLayout>
+  );
 }
